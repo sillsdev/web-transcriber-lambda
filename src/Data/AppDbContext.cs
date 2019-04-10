420f0f092ed096dfe4eb6665b232a9c71bfdd725
Sara Hentzel <sara_hentzel@sil.org>
2019-04-10 11:03:44 -0500

    projecttypes, updated dlls

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -2,6 +2,10 @@
 
 using Microsoft.EntityFrameworkCore;
 using SIL.Transcriber.Models;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SIL.Transcriber.Data
 {
@@ -42,6 +46,34 @@ namespace SIL.Transcriber.Data
                 }
             }
         }
+        //// https://benjii.me/2014/03/track-created-and-modified-fields-automatically-with-entity-framework-code-first/
+        private void AddTimestamps()
+        {
+            var entries = ChangeTracker.Entries().Where(e => e.Entity is ITrackDate && (e.State == EntityState.Added || e.State == EntityState.Modified));
+            DateTime now = DateTime.UtcNow;
+            foreach (var entry in entries)
+            {
+                if (entry.Entity is ITrackDate trackDate)
+                {
+                    if (entry.State == EntityState.Added)
+                    {
+                        trackDate.DateCreated = now;
+                    }
+                    trackDate.DateUpdated = now;
+                }
+            }
+        }
+        public override int SaveChanges()
+        {
+            AddTimestamps();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            AddTimestamps();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
 
         public  DbSet<Book> Books { get; set; }
         public  DbSet<BookType> Booktypes { get; set; }
@@ -50,13 +82,13 @@ namespace SIL.Transcriber.Data
         public  DbSet<OrganizationMembership> Organizationmemberships { get; set; }
         public  DbSet<Project> Projects { get; set; }
         public  DbSet<ProjectIntegration> Projectintegrations { get; set; }
-        public  DbSet<ProjectType> ProjectTypes { get; set; }
+        public  DbSet<ProjectType> Projecttypes { get; set; }
         public  DbSet<ProjectUser> Projectusers { get; set; }
         public  DbSet<Reviewer> Reviewers { get; set; }
         public  DbSet<Role> Roles { get; set; }
         public  DbSet<Set> Sets { get; set; }
         public  DbSet<TaskMedia> Taskmedia { get; set; }
-        public  DbSet<Task> Tasks { get; set; }
+        public  DbSet<SIL.Transcriber.Models.Task> Tasks { get; set; }
         public  DbSet<TaskSet> Tasksets { get; set; }
         public  DbSet<TaskState> Taskstates { get; set; }
         public DbSet<UserRole> Userroles { get; set; }
