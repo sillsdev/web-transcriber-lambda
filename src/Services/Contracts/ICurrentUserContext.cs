775d6d0e44412e2c67144aca3111778626fad727
Sara Hentzel <sara_hentzel@sil.org>
2019-04-12 09:04:02 -0500

    JWT

---
+++ src/Services/Contracts/ICurrentUserContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SIL.Transcriber.Models;
+
+namespace SIL.Transcriber.Services
+{
+    public interface ICurrentUserContext
+    {
+        string Auth0Id { get; }
+        string Email { get; }
+        string GivenName { get; }
+        string FamilyName { get; }
+        string Name { get; }
+    }
+}
