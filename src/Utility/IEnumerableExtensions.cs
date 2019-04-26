5dad88ff8f3a0acbd1721d60b80d0aa241a229da
Sara Hentzel <sara_hentzel@sil.org>
2019-04-26 12:26:58 -0500

    Scriptoria organizations/groups/users/projects - missed utility functions

---
+++ src/Utility/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SIL.Transcriber.Utility
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> OrEmpty<T>(this IEnumerable<T> list)
+        {
+            return list ?? Enumerable.Empty<T>();
+        }
+    }
+}
