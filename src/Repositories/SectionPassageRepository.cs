28e092028248c4a6b4bc2029c7a83860de0e1d2c
sarahentzel <sara_hentzel@sil.org>
2020-05-06 18:21:53 -0500

    remove sqlHelper, use UpdateRange

--- src/Repositories/SectionPassageRepository.cs
+++ src/Repositories/SectionPassageRepository.cs
@@ -1,11 +1,7 @@
 ﻿
 using JsonApiDotNetCore.Data;
 using JsonApiDotNetCore.Services;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.Logging;
-using Npgsql;
-using PostgreSQLCopyHelper;
-using SIL.Transcriber.Data;
 using SIL.Transcriber.Models;
 using SIL.Transcriber.Utility;
 using System;
@@ -66,100 +62,16 @@ namespace SIL.Transcriber.Repositories
             foreach (Passage p in passages)
                 SetModifiedValues(p, user, now, origin, setCreated);
         }
-        private void SetIds(NpgsqlConnection con, List<Section> sections)
-        {
-            string sql = "SELECT nextval('section_id_seq')";
-            using (NpgsqlCommand cmd = new NpgsqlCommand(sql, con))
-            {
-                foreach (Section s in sections)
-                {
-                    s.Id = (int)(long)cmd.ExecuteScalar();
-                }
-            }
-        }
-        private void SetIds(NpgsqlConnection con, List<Passage> passages)
-        {
-            string sql = "SELECT nextval('passages_id_seq')";
-            using (NpgsqlCommand cmd = new NpgsqlCommand(sql, con))
-            {
-                foreach (Passage p in passages)
-                {
-                     p.Id = (int)(long)cmd.ExecuteScalar();
-                }
-            }
-        }
-        public List<Section> BulkInsertSections(NpgsqlConnection connection, List<Section> sections)
-        {
-            SetIds(connection, sections);
-            SetModifiedValues(sections, true);
-            PostgreSQLCopyHelper<Section> copyHelper = new PostgreSQLCopyHelper<Section>("public", "sections")
-                .MapInteger("id", x => x.Id)
-                .MapText("name", x => x.Name)
-                .MapInteger("planid", x => x.PlanId)
-                .MapInteger("sequencenum", x => x.Sequencenum)
-                .MapText("state", x => x.State)
-                .MapTimeStamp("datecreated", x => x.DateCreated)
-                .MapTimeStamp("dateupdated", x => x.DateUpdated)
-                .MapBoolean("archived", x => x.Archived)
-                .MapInteger("lastmodifiedby", x => x.LastModifiedBy)
-                .MapText("lastmodifiedorigin", x => x.LastModifiedOrigin)
-                .MapInteger("editorid", x => x.EditorId)
-                .MapInteger("transcriberid", x => x.TranscriberId);
 
-            Logger.LogInformation($"Insert Sections {sections.Count} {sections[0].PlanId}");
-            //dbContext.BulkInsert(sections);
-            copyHelper.SaveAll(connection, sections);
-            return sections;
-        }
         public List<Section> BulkUpdateSections(List<Section> sections)
         {
             dbContext.UpdateRange(sections);
             dbContext.SaveChanges();
             return sections;
-            //SetModifiedValues(sections, false);
-            //dbContext.BulkUpdate(sections);
-            //return sections;
         }
-        private static void DisplayStates(IEnumerable<EntityEntry> entries)
-        {
-            foreach (var entry in entries)
-            {
-                Console.WriteLine($"Entity: {entry.Entity.GetType().Name}, State: { entry.State.ToString()} ");
-            }
-        }
-        public List<Passage> BulkInsertPassages(List<Passage> passages)
-        {
-            dbContext.UpdateRange(passages);
-            DisplayStates(dbContext.ChangeTracker.Entries());
-            dbContext.SaveChanges();
-            return passages;
-        }
-        public List<Passage> BulkInsertPassages(NpgsqlConnection connection, List<Passage> passages)
-        {
-            SetIds(connection, passages);
-            SetModifiedValues(passages, true);
-            PostgreSQLCopyHelper<Passage> copyHelper = new PostgreSQLCopyHelper<Passage>("public", "sections")
-               .MapInteger("id", x => x.Id)
-               .MapText("title", x => x.Title)
-               .MapText("book", x => x.Book)
-               .MapText("reference", x => x.Reference)
-               .MapInteger("sequencenum", x => x.Sequencenum)
-               .MapText("state", x => x.State)
-               .MapTimeStamp("datecreated", x => x.DateCreated)
-               .MapTimeStamp("dateupdated", x => x.DateUpdated)
-               .MapBoolean("archived", x => x.Archived)
-               .MapInteger("lastmodifiedby", x => x.LastModifiedBy)
-               .MapText("lastmodifiedorigin", x => x.LastModifiedOrigin);
 
-            Logger.LogInformation($"Insert Passages {passages.Count} {passages[0]}");
-            //dbContext.BulkInsert(passages);
-            copyHelper.SaveAll(connection, passages);
-            return passages;
-        }
         public List<Passage> BulkUpdatePassages(List<Passage> passages)
         {
-            //SetModifiedValues(passages, false);
-            //dbContext.BulkUpdate(passages);
             dbContext.UpdateRange(passages);
             dbContext.SaveChanges();
             return passages;
--- src/Services/SectionPassageService.cs
+++ src/Services/SectionPassageService.cs
@@ -9,8 +9,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using JsonApiDotNetCore.Internal;
 using SIL.Transcriber.Repositories;
-using Npgsql;
-using static SIL.Transcriber.Utility.EnvironmentHelpers;
 using Microsoft.EntityFrameworkCore.Storage;
 
 namespace SIL.Transcriber.Services
@@ -61,42 +59,20 @@ namespace SIL.Transcriber.Services
                 if (ex.InnerException != null && ex.InnerException.Message.Contains("23505"))
                     return null;
             }
-            var connection = new NpgsqlConnection(GetVarOrDefault("SIL_TR_CONNECTIONSTRING", ""));
-            connection.Open();
-
-           // using (var transaction = connection.BeginTransaction()) 
             using (IDbContextTransaction transaction = MyRepository.BeginTransaction())
             {
                 try
                 {
-                    //IEnumerable<JToken> newsecs = data.Where(d => d["id"] == null && (bool)d["issection"] && (bool)d["changed"]);
-                    //IEnumerable<JToken> updsecs = data.Where(d => d["id"] != null && (bool)d["issection"] && (bool)d["changed"]);
                     IEnumerable<JToken> updsecs = data.Where(d =>(bool)d["issection"] && (bool)d["changed"]);
                     //add all sections
-                    //List<Section> newsections = new List<Section>();
                     List<Section> updsections = new List<Section>();
-                    //foreach (JToken item in newsecs)
-                    //{
-                        //newsections.Add(new Section(item, entity.PlanId));
-                    //}
+
                     foreach (JToken item in updsecs)
                     {
                         updsections.Add(item["id"] != null ? MyRepository.GetSection((int)item["id"]).UpdateFrom(item) : new Section(item, entity.PlanId));
-                    }/*
-                    if (newsections.Count > 0)
-                    {
-                        Logger.LogInformation($"newsections {newsections.Count} {newsections}");
-                        MyRepository.BulkInsertSections(connection, newsections);
-                        int ix = 0;
-                        foreach (JToken item in newsecs)
-                        {
-                            item["id"] = newsections[ix].Id;
-                            ix++;
-                        }
-                    } */
+                    }
                     if (updsections.Count > 0)
                     {
-                        Logger.LogInformation($"updsections {updsections.Count} {updsections}");
                         MyRepository.BulkUpdateSections(updsections);
                         int ix = 0;
                         foreach (JToken item in updsecs)
@@ -107,7 +83,6 @@ namespace SIL.Transcriber.Services
                     }
                     int lastSectionId = 0;
                     /* process all the passages now */
-                    List<Passage> newpassages = new List<Passage>();
                     List<Passage> updpassages = new List<Passage>();
                     foreach (JToken item in data)
                     {
@@ -117,32 +92,15 @@ namespace SIL.Transcriber.Services
                         }
                         else if ((bool)item["changed"])
                         {
-                            if (item["id"] == null)
-                                updpassages.Add(new Passage(item, lastSectionId));
-                            else
-                                updpassages.Add(MyRepository.GetPassage((int)item["id"]).UpdateFrom(item));
+                           updpassages.Add(item["id"] != null ? MyRepository.GetPassage((int)item["id"]).UpdateFrom(item): new Passage(item, lastSectionId));
                         }
                     }
-                    if (newpassages.Count > 0)
+                     if (updpassages.Count > 0)
                     {
-                        IEnumerable<JToken> newpsgs = data.Where(d => d["id"] == null && !(bool)d["issection"]);
-
-                        Logger.LogInformation($"newpassages {newpassages.Count} {newpassages}");
-                        MyRepository.BulkInsertPassages(newpassages);
-                        int ix = 0;
-                        foreach (JToken item in newpsgs)
-                        {
-                            item["id"] = newpassages[ix].Id;
-                            ix++;
-                        }
-                    }
-                    if (updpassages.Count > 0)
-                    {
-                        Logger.LogInformation($"updpassages {updpassages.Count} {updpassages}");
+                        //Logger.LogInformation($"updpassages {updpassages.Count} {updpassages}");
                         MyRepository.BulkUpdatePassages(updpassages);
                     }
-                    Logger.LogInformation($"Success! committing...{data}");
-
+                    
                     transaction.Commit();
                     entity.Data = JsonConvert.SerializeObject(data);
                     entity.Complete = true;
