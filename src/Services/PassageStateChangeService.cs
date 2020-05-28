a54ba76a8a15a5d858562a19611b2e83fc315d8d
sarahentzel <47537281+sarahentzel@users.noreply.github.com>
2020-05-28 14:52:23 -0500

    TT_1438_1439 create history when synced.  State to done. (#49)

--- src/Services/PassageStateChangeService.cs
+++ src/Services/PassageStateChangeService.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using JsonApiDotNetCore.Data;
 using JsonApiDotNetCore.Services;
 using Microsoft.Extensions.Logging;
 using SIL.Transcriber.Models;
+using SIL.Transcriber.Repositories;
 using static SIL.Transcriber.Utility.ServiceExtensions;
 
 namespace SIL.Transcriber.Services
 {
     public class PassageStateChangeService : BaseService<PassageStateChange>
     {
-
         public PassageStateChangeService(
             IJsonApiContext jsonApiContext,
             IEntityRepository<PassageStateChange> repository,
@@ -22,6 +23,13 @@ namespace SIL.Transcriber.Services
             return await GetScopedToCurrentUser(
                 base.GetAsync,
                 JsonApiContext);
+        }
+        public Task<PassageStateChange> CreateAsync(Passage passage)
+        {
+            return base.CreateAsync(new PassageStateChange { 
+                PassageId = passage.Id, 
+                State = passage.State,
+                Comments = passage.LastComment});
         }
     }
     
