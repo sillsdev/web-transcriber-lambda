0ae236889aa9d371a0ef4f0a31ce3aee65b1921d
sarahentzel <sara_hentzel@sil.org>
2020-07-01 10:07:25 -0500

    section addNumbers

--- src/Models/SectionSummary.cs
+++ src/Models/SectionSummary.cs
@@ -8,6 +8,6 @@
         public int endChapter { get; set; } 
         public int startVerse { get; set; }
         public int endVerse { get; set; }
-        public string SectionHeader(bool addNumbers = true) { return addNumbers ? section.Sequencenum.ToString() + " - " : "" + section.Name; }
+        public string SectionHeader(bool addNumbers = true) { return (addNumbers ? section.Sequencenum.ToString() + " - " : "") + section.Name; }
     }
 }
