c520d1d49672a468e31e8fe8f5a7b02a521b7f9f
Sara Hentzel <sara_hentzel@sil.org>
2021-06-02 12:59:54 -0500

    remove unused auth functions

--- src/Services/AuthService.cs
+++ src/Services/AuthService.cs
@@ -56,46 +56,6 @@ namespace SIL.Transcriber.Services
         throw new Exception("not implemented");
         }
 
-        private async Task<(string AccessToken, bool Refreshed)> GetAccessTokenAsync()
-        {
-            await _lock.WaitAsync();
-            try
-            {
-                if (!IsAccessTokenExpired())
-                    return (_accessToken, false);
-
-                string clientId = GetVarOrDefault("SIL_TR_AUTH0_TOKEN_ACCESS_CLIENT_ID", "");
-                string clientSecret = GetVarOrDefault("SIL_TR_AUTH0_TOKEN_ACCESS_CLIENT_SECRET", "");
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "oauth/token");
-
-                JObject requestObj = new JObject(
-                    new JProperty("grant_type", "client_credentials"),
-                    new JProperty("client_id", clientId),
-                    new JProperty("client_secret", clientSecret),
-                    new JProperty("audience", _httpClient.BaseAddress + "api/v2/"));
-                request.Content = new StringContent(requestObj.ToString(), Encoding.UTF8, "application/json");
-                HttpResponseMessage response = await _httpClient.SendAsync(request);
-                response.EnsureSuccessStatusCode();
-                string responseJson = await response.Content.ReadAsStringAsync();
-                dynamic responseObj = JObject.Parse(responseJson);
-                _accessToken = (string)responseObj.access_token;
-                return (_accessToken, true);
-            }
-            finally
-            {
-                _lock.Release();
-            }
-        }
-
-        private bool IsAccessTokenExpired()
-        {
-            if (_accessToken == null)
-                return true;
-            JwtSecurityToken accessToken = new JwtSecurityToken(_accessToken);
-            DateTime now = DateTime.UtcNow;
-            return now < accessToken.ValidFrom || now > accessToken.ValidTo;
-        }
-
         protected override void DisposeManagedResources()
         {
             _httpClient.Dispose();
