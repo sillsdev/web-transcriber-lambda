6416210c29cbc5122cac3993e9953de46dcdf2ee
Sara Hentzel <sara_hentzel@sil.org>
2022-03-08 19:56:21 -0600

    TT-3146 resolved not passed back!

--- src/Models/Discussion.cs
+++ src/Models/Discussion.cs
@@ -14,6 +14,7 @@ namespace SIL.Transcriber.Models
         [Attr("segments")]
         [Column(TypeName = "jsonb")]
         public string Segments { get; set; }
+        [Attr("resolved")]
         public bool Resolved { get; set; }
         public int? RoleId { get; set; }
         [HasOne("role", Link.None)]
