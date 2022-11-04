fd46cd81263d1c6800fd5833a3d7cf421acf50a8
Sara Hentzel <sara_hentzel@sil.org>
2022-11-04 11:13:19 -0500

    TT-4663 RecordedbyUser

--- src/Services/OfflineDataService.cs
+++ src/Services/OfflineDataService.cs
@@ -1594,7 +1594,7 @@ namespace SIL.Transcriber.Services
                     report.Add(MediafileChangesReport(existing, importing));
                 existing.Link = importing.Link != null ? importing.Link : false;
                 existing.Position = importing.Position;
-                existing.RecordedbyUserId = importing.RecordedbyUserId;
+                existing.RecordedbyUser = importing.RecordedbyUser;
                 existing.PerformedBy = importing.PerformedBy;
                 existing.Segments = importing.Segments;
                 existing.SourceSegments = importing.SourceSegments;
@@ -2051,7 +2051,8 @@ namespace SIL.Transcriber.Services
                                                     PlanId = m.PlanId,
                                                     Position = m.Position,
                                                     ReadyToShare = m.ReadyToShare,
-                                                    RecordedbyUserId = m.RecordedbyUserId,
+                                                   // RecordedbyuserId = m.RecordedbyuserId,
+                                                    RecordedbyUser = m.RecordedbyUser,
                                                     ResourcePassageId = m.ResourcePassageId,
                                                     S3File = m.S3File,
                                                     Segments = m.Segments,
