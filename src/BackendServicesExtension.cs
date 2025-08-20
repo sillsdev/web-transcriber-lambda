898638da0e0e98e702b1c827a1ae2caee6c4db7c
Sara Hentzel <sara_hentzel@sil.org>
2025-08-20 14:12:48 -0500

    update version

--- src/BackendServicesExtension.cs
+++ src/BackendServicesExtension.cs
@@ -245,7 +245,7 @@ namespace SIL.Transcriber
                     "v1",
                     new OpenApiInfo
                     {
-                        Version = "v4.2",
+                        Version = "v4.3.1",
                         Title = "Transcriber API",
                         Contact = new OpenApiContact
                         {
